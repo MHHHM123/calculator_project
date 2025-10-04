a5d8ef2dda6bd25f193c18ddbed767b101e2f1c3
MHHHM123 <mhmdmuradm@gmail.com>
2025-10-04 16:37:26 +0000

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,32 @@
+const display=document.getElementById("display");
+
+function appendtodispaly(input){
+    if(display.value=="ERROR"){
+        display.value="";
+    }
+    display.value+=input;
+}
+
+function clearDisplay(){
+    display.value='';
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value);
+    }
+    catch(error){
+        
+        display.value="ERROR";
+        
+        setTimeout(()=>{
+            display.value='';
+        }, 2400);
+        
+        
+
+    }
+
+    
+
+}
